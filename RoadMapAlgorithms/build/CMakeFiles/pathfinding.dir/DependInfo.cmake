644c63d8d65bdf510b08004a5e3c0e8775e62932
VladimirDultsev <dultseffvladim@yandex.ru>
2026-01-28 16:30:23 +0300

    Refactored FifteesAlgorithms

---
+++ RoadMapAlgorithms/build/CMakeFiles/pathfinding.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vladimir/CLionProject/GraphAlgorithms/RoadMapAlgorithms/algorithms/astar.cpp" "CMakeFiles/pathfinding.dir/algorithms/astar.cpp.o" "gcc" "CMakeFiles/pathfinding.dir/algorithms/astar.cpp.o.d"
+  "/Users/vladimir/CLionProject/GraphAlgorithms/RoadMapAlgorithms/algorithms/bidirectional_astar.cpp" "CMakeFiles/pathfinding.dir/algorithms/bidirectional_astar.cpp.o" "gcc" "CMakeFiles/pathfinding.dir/algorithms/bidirectional_astar.cpp.o.d"
+  "/Users/vladimir/CLionProject/GraphAlgorithms/RoadMapAlgorithms/algorithms/dijkstra.cpp" "CMakeFiles/pathfinding.dir/algorithms/dijkstra.cpp.o" "gcc" "CMakeFiles/pathfinding.dir/algorithms/dijkstra.cpp.o.d"
+  "/Users/vladimir/CLionProject/GraphAlgorithms/RoadMapAlgorithms/algorithms/multithread_bidirectional_astar.cpp" "CMakeFiles/pathfinding.dir/algorithms/multithread_bidirectional_astar.cpp.o" "gcc" "CMakeFiles/pathfinding.dir/algorithms/multithread_bidirectional_astar.cpp.o.d"
+  "/Users/vladimir/CLionProject/GraphAlgorithms/RoadMapAlgorithms/main.cpp" "CMakeFiles/pathfinding.dir/main.cpp.o" "gcc" "CMakeFiles/pathfinding.dir/main.cpp.o.d"
+  "/Users/vladimir/CLionProject/GraphAlgorithms/RoadMapAlgorithms/test_system.cpp" "CMakeFiles/pathfinding.dir/test_system.cpp.o" "gcc" "CMakeFiles/pathfinding.dir/test_system.cpp.o.d"
+  "/Users/vladimir/CLionProject/GraphAlgorithms/RoadMapAlgorithms/utils/distance.cpp" "CMakeFiles/pathfinding.dir/utils/distance.cpp.o" "gcc" "CMakeFiles/pathfinding.dir/utils/distance.cpp.o.d"
+  "/Users/vladimir/CLionProject/GraphAlgorithms/RoadMapAlgorithms/utils/graph.cpp" "CMakeFiles/pathfinding.dir/utils/graph.cpp.o" "gcc" "CMakeFiles/pathfinding.dir/utils/graph.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ RoadMapAlgorithms/build/CMakeFiles/pathfinding.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pathfinding.dir/algorithms/astar.cpp.o"
+  "CMakeFiles/pathfinding.dir/algorithms/astar.cpp.o.d"
+  "CMakeFiles/pathfinding.dir/algorithms/bidirectional_astar.cpp.o"
+  "CMakeFiles/pathfinding.dir/algorithms/bidirectional_astar.cpp.o.d"
+  "CMakeFiles/pathfinding.dir/algorithms/dijkstra.cpp.o"
+  "CMakeFiles/pathfinding.dir/algorithms/dijkstra.cpp.o.d"
+  "CMakeFiles/pathfinding.dir/algorithms/multithread_bidirectional_astar.cpp.o"
+  "CMakeFiles/pathfinding.dir/algorithms/multithread_bidirectional_astar.cpp.o.d"
+  "CMakeFiles/pathfinding.dir/main.cpp.o"
+  "CMakeFiles/pathfinding.dir/main.cpp.o.d"
+  "CMakeFiles/pathfinding.dir/test_system.cpp.o"
+  "CMakeFiles/pathfinding.dir/test_system.cpp.o.d"
+  "CMakeFiles/pathfinding.dir/utils/distance.cpp.o"
+  "CMakeFiles/pathfinding.dir/utils/distance.cpp.o.d"
+  "CMakeFiles/pathfinding.dir/utils/graph.cpp.o"
+  "CMakeFiles/pathfinding.dir/utils/graph.cpp.o.d"
+  "pathfinding"
+  "pathfinding.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pathfinding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
